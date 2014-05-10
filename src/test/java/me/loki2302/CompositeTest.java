a243d192385d647063b8d9e5ecfeab99597d65d9
loki2302 <loki2302@loki2302.me>
2014-05-10 06:07:01 -0700

    Added update scenario check to composite test

--- src/test/java/me/loki2302/CompositeTest.java
+++ src/test/java/me/loki2302/CompositeTest.java
@@ -2,6 +2,7 @@ package me.loki2302;
 
 import me.loki2302.entities.ChangeLogTransaction;
 import me.loki2302.entities.CreateEntityChangeLogEvent;
+import me.loki2302.entities.UpdateEntityChangeLogEvent;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.annotation.DirtiesContext;
@@ -38,7 +39,13 @@ public class CompositeTest extends AbstractIntegrationTest {
         assertEquals(2, transactions.size());
         assertTransactionHasSingleCreateEntityEventForNote(transactions.get(1), "note2", "bye");
 
-        // TODO: update note #1
+        noteOperations.updateNote("note1", "omg");
+
+        transactions = transactionOperations.getAllTransactions();
+        assertEquals(3, transactions.size());
+        assertTransactionHasSingleUpdateEntityEventForNote(transactions.get(2), "note1", "hello", "omg");
+
+
         // TODO: delete note #2
     }
 
@@ -55,5 +62,23 @@ public class CompositeTest extends AbstractIntegrationTest {
         assertEquals(id, changeLogEvent.entityId);
         assertEquals("text", changeLogEvent.properties.get(0).name);
         assertEquals(text, changeLogEvent.properties.get(0).value);
+    }
+
+    private static void assertTransactionHasSingleUpdateEntityEventForNote(
+            ChangeLogTransaction transaction,
+            String id,
+            String oldText,
+            String newText) {
+
+        assertEquals(1, transaction.events.size());
+        assertTrue(transaction.events.get(0) instanceof UpdateEntityChangeLogEvent);
+
+        UpdateEntityChangeLogEvent changeLogEvent = (UpdateEntityChangeLogEvent)transaction.events.get(0);
+        assertEquals("me.loki2302.entities.Note", changeLogEvent.entityName);
+        assertEquals(id, changeLogEvent.entityId);
+        assertEquals("text", changeLogEvent.properties.get(0).name);
+        assertEquals(newText, changeLogEvent.properties.get(0).value);
+        assertEquals("text", changeLogEvent.oldProperties.get(0).name);
+        assertEquals(oldText, changeLogEvent.oldProperties.get(0).value);
     }
 }
